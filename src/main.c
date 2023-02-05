bc73c3843d9815a1aa7a0d4bc4e772c8eff6543f
21herbertm <53823046+21herbertm@users.noreply.github.com>
2023-02-04 23:28:01 -0500

    Problem 2 Toggle an LED

--- src/main.c
+++ src/main.c
@@ -1,28 +1,34 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #define F_CPU 16000000UL
+
 void Initialize()
 {
-    DDRB |= (1<<DDB4);
-    DDRB |= (1<<DDB3);
-    DDRB |= (1<<DDB2);
-    DDRB |= (1<<DDB1);
-}
+    cli();
+    //************ PIN ALLOCATION ***************
+    //TOGGLES AN LED CODE
+    DDRB |= (1<<DDB5); //SET PB5 TO BE OUTPUT PIN
+    //PB5 IS THE  SAME
+    // HIGH, LED ON
+    //LOW, LED OFF
+    DDRD &= ~(1<<DDB7); //SET PB7 TO BE INPUT PIN
+    //INSTEAD OF PD5 WE USE PB7
+    //NEEDS TO BE PULLED HIGH
+    //WHEN SWITCH IS PRESSED, VOLTAGE AT PD5 WILL BE 0V
+    PORTD |= (1<<PORTD5); // ENABLE PULL UP RESISTOR ON PD5
+    //PD5 IS THE SAME
+    //********** PIN CHANGE INTERRUPT SETUP *************
+    PCICR |= (1<<PCIE2); // ENABLE PCINT21 PIN CHANGE INTERRUPT
+    PCMSK2 |= (1<<PCINT21); //ENABLE TRIGGER FOR PCINT21
+    sei();
+} //INITIALIZE
+ISR(PCINT2_vect)
+    {
+    PORTB ^=(1<<PORTB5); // TOGGLE PB5
+    } //ISR
 int main(void)
 {
     Initialize();
-
-    while(1)
-    {
-        PORTB |= (1<<PORTB4);
-        PORTB |= (1<<PORTB3);
-        PORTB |= (1<<PORTB2);
-        PORTB |= (1<<PORTB1);
-        _delay_ms(1000);
-        PORTB &= ~(1<<PORTB4);
-        PORTB &= ~(1<<PORTB3);
-        PORTB &= ~(1<<PORTB2);
-        PORTB &= ~(1<<PORTB1);
-        _delay_ms(1000);
-    }
-}
+    while(1);
+} // MAIN
